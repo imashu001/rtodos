f09f7918725d0e666b317f1ce2156b2fd2fc92ef
imashu001 <imashu001@gmail.com>
2020-10-16 14:05:53 +0530

    Updates

---
+++ precache-manifest.9433abd634728bc144a79cde96653198.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f643bcb63e78bccb74ab5e5512403936",
+    "url": "/rtodos/index.html"
+  },
+  {
+    "revision": "1b986303b7ce51da60cf",
+    "url": "/rtodos/static/css/main.e6ed0dc3.chunk.css"
+  },
+  {
+    "revision": "ebc9b4e4b9632889aaa8",
+    "url": "/rtodos/static/js/2.ac6fb4b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rtodos/static/js/2.ac6fb4b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b986303b7ce51da60cf",
+    "url": "/rtodos/static/js/main.a302aeff.chunk.js"
+  },
+  {
+    "revision": "acd2b57a3b8690d129b0",
+    "url": "/rtodos/static/js/runtime-main.bd04c991.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rtodos/static/media/logo.5d5d9eef.svg"
+  }
+]);
